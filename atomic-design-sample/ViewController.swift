c8c15e03835d693466e90c248f3634b01eeb3496
hicka04 <calpissoda8221@gmail.com>
2018-12-29 12:04:21 +0900

    プロジェクト追加

---
+++ atomic-design-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  atomic-design-sample
+//
+//  Created by hicka04 on 2018/12/29.
+//  Copyright © 2018 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
